cbd5d4afd6cf267a12fdb0239c3457d63e921b40
Cheng Huang <daily2432121@gmail.com>
2014-09-01 11:03:12 -0400

    Init

---
+++ VSProject/BiaoProject/BiaoProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiaoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
