053bcd970b21ddf7b105eeef0facb39af2b9fc62
Cheng Huang <cheng.huang@finoconsulting.com>
2014-09-05 15:54:00 -0400

    Csv format check

--- VSProject/BiaoProject/BiaoProject.Service/Helper/ExcelHelper.cs
+++ VSProject/BiaoProject/BiaoProject.Service/Helper/ExcelHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +22,12 @@ namespace BiaoProject.Service.Helper
 
     public class ExcelHelper
     {
+        public class CsvCheckResult
+        {
+            public bool Success { get; set; }
+            public string ErrorMsg { get; set; }
+        }
+
 
         public List<T> GetAllFromCsv<T>(string fileName, List<ColumnMapping> columnMapping  ) where T:new ()
         {
@@ -47,6 +54,51 @@ namespace BiaoProject.Service.Helper
                     result.Add(newItem);
                 }
             }
+            return result;
+        }
+
+        public CsvCheckResult CheckCsv<T>(string fileName, List<ColumnMapping> columnMapping) where T : new()
+        {
+            CsvCheckResult result = new CsvCheckResult();
+            
+            var dict = columnMapping.ToDictionary(c => c.ColumnName.ToLower());
+            using (StreamReader readFile = new StreamReader(fileName))
+            {
+                
+                string rawHeader = readFile.ReadLine();
+                var headers = rawHeader.ToLower().Split(',').Select(e => e.Trim().ToLower()).ToList();
+                if (headers.Count() != columnMapping.Count())
+                {
+                    result.Success = false;
+                    result.ErrorMsg = "Csv file contains different number of rows.";
+                    return result;
+                }
+                
+                foreach (string header in headers)
+                {
+                    if (!dict.ContainsKey(header))
+                    {
+                        result.Success = false;
+                        result.ErrorMsg = string.Format("Column {0} is not recognized.", header);
+                        return result;
+                    }
+                }
+                string line;
+                int count = 0;
+                while ((line = readFile.ReadLine()) != null)
+                {
+                    count++;
+                    var row = line.Split(',');
+                    if (row.Count() != headers.Count)
+                    {
+                        result.Success = false;
+                        result.ErrorMsg = string.Format("Data is missing in line {0} in csv file", count++);
+                        return result;
+                    }
+                }
+            }
+            result.Success = true;
+            result.ErrorMsg = null;
             return result;
         }
     }
